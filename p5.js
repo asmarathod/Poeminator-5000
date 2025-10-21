59a5e9dca3772bf83324b7fba85995968f20e626
asmarathod <156235413+asmarathod@users.noreply.github.com>
2025-10-20 20:25:29 -0400

    Create p5.js
    there is an issue here where it's not running properly but I will go back and update the file with the correct code once I fix it.

---
+++ p5.js
@@ -0,0 +1,30 @@
+let title;
+
+
+function setup() {
+  noCanvas();
+  
+ const controls = createDiv().addClass('controls');
+ title = createP("Doofenshmirtz Poeminator 5000")
+ poem = createDiv('');
+  textbox = createInput('').attribute('type','text').parent(controls);
+  addBtn = createButton('Add Line').parent(controls);
+  
+  addBtn.mousePressed(() => {
+    let userLine = textbox.value().trim();
+    if (!userLine) return;
+    textbox.value("");
+                  
+    let row = createDiv().addClass('poem-row').parent(poem);
+    let textSpan = createSpan(userLine).parent(row);
+    let slider = createSlider(0,360, floor(random(360)), 1).parent(row);
+    
+    let applyColor = () => textSpan.style('color', 'hsl($(slider.value()), 80%, 50%)');
+    slider.input(applyColor);
+    applyColor();
+  })
+}
+
+function draw() {
+  background(220);
+}
